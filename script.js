62f6449fb05f152b10d8c5bf568a71c1322b8fdc
marwaaa1508 <marwaibthisam@gmail.com>
2026-06-10 19:57:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 let player;
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '0',
-    width: '0',
-    videoId: 'uaKyS6gqTxI',
-    events: {
-      'onReady': () => {}
-    }
+function onYouTubeIframeAPIReady(){
+  player = new YT.Player('player',{
+    height:'0',
+    width:'0',
+    videoId:'uaKyS6gqTxI'
   });
 }
 
 document.getElementById("saveDateBtn").addEventListener("click", () => {
 
-  // 🎵 start music
-  if (player) {
+  // 🎵 play music
+  if(player){
     player.playVideo();
   }
 
-  // 🌸 smooth scroll
-  document.getElementById("invitation").scrollIntoView({
-    behavior: "smooth"
+  // ⬇️ scroll to section 2
+  document.getElementById("invite").scrollIntoView({
+    behavior:"smooth"
   });
 
 });
